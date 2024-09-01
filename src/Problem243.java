eeb41dd4763e21a7661e704c2c95401e41bd6a50
aliaksei <aliaksmarchuk@gmail.com>
2024-09-02 00:37:55 +0300

    solved problem 243

---
+++ src/Problem243.java
@@ -0,0 +1,22 @@
+public class Problem243 {
+
+    public int shortestDistance(String[] words, String word1, String word2){
+        int result = Integer.MAX_VALUE;
+        int first = -1;
+        int second = -1;
+        for(int i = 0; i < words.length; i++){
+            if(words[i].equals(word1)){
+                first = i;
+            }
+            if (words[i].equals(word2)){
+                second = i;
+            }
+            if(first != -1 && second != -1){
+                result = Math.min(second - first, result);
+            }
+        }
+
+        return result;
+    }
+
+}
