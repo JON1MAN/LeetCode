037ad2d6cadba4cc49612d728bc58476061992cd
aliaksei <aliaksmarchuk@gmail.com>
2024-09-01 17:23:57 +0300

    solved problem 125

---
+++ src/Problem125.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+
+public class Problem125 {
+
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase(Locale.ROOT);
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(Character.isAlphabetic(c) || Character.isDigit(c)){
+                builder.append(c);
+            }
+        }
+        s = builder.toString();
+        int left = 0;
+        int right = builder.length() - 1;
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Problem125 problem125 = new Problem125();
+        System.out.println(problem125.isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println(problem125.isPalindrome("raceacar"));
+    }
+
+}
