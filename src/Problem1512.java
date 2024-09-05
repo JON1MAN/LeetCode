97d7f837fb41deee269b8715a9b9a15c60f69d4c
aliaksei <aliaksmarchuk@gmail.com>
2024-09-05 16:02:07 +0200

    solved 1512

---
+++ src/Problem1512.java
@@ -0,0 +1,20 @@
+public class Problem1512 {
+
+    public int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        int[] repeat = new int[101];
+
+        for(int number : nums){
+            count += repeat[number]++;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,1,1,1};
+        Problem1512 problem1512 = new Problem1512();
+        System.out.println(problem1512.numIdenticalPairs(nums));
+    }
+
+}
