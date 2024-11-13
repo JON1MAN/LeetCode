b3335c9122a6438d013bc235964646eefd34027f
aliaksei <aliaksmarchuk@gmail.com>
2024-11-13 02:44:22 +0100

    changes of problem2070

--- src/Problem2070.java
+++ src/Problem2070.java
@@ -1,40 +1,57 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Problem2070 {
 
-    public static int[] maximumBeauty(int[][] items, int[] queries) {
-        int n = items.length;
+    public int[] maximumBeauty(int[][] items, int[] queries) {
+        int n = queries.length;
         int[] answer = new int[n];
-        Map<Integer, Integer> map = new LinkedHashMap<>();
 
+        Arrays.sort(queries);
+        Arrays.sort(items, Comparator.comparingInt(a -> a[0]));
 
         for (int[] item : items) {
-            int key = item[0];
-            int value = item[1];
-            map.put(key, Math.max(map.getOrDefault(key, 0), value));
+            System.out.println(Arrays.toString(item));
         }
 
-        for(int i = 0; i < queries.length; i++) {
-            int price = queries[i];
-            queries[i] = 0;
-            for (Map.Entry<Integer, Integer> set : map.entrySet()) {
-                int key = set.getKey();
-                int value = set.getValue();
+        for (int i = 1; i < items.length - 1; i++) {
+            items[i][1] = Math.max(items[i - 1][1], items[i][1]);
+        }
 
-                if (key <= price) {
-                    queries[i] = Math.max(value, queries[i]);
-                }
+        for (int[] item : items) {
+            System.out.println(Arrays.toString(item));
+        }
+
+        for (int i = 0; i <= n - 1; i++) {
+            int maxBeauty = binarySearch(items, queries[i]);
+            answer[i] = maxBeauty;
+        }
+
+        return answer;
+    }
+
+    public int binarySearch(int[][] array, int price) {
+        int maxBeauty = 0;
+        int left = 0;
+        int right = array.length - 1;
+        int mid = 0;
+
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (array[mid][0] <= price) {
+                maxBeauty = Math.max(maxBeauty, array[mid][1]);
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        return queries;
-     }
+
+        return maxBeauty;
+    }
 
     public static void main(String[] args) {
+        Problem2070 problem2070 = new Problem2070();
         int[][] items = {{1,2}, {3,2}, {2,4}, {5,6}, {3,5},};
-        int[] result = Problem2070.maximumBeauty(items, new int[]{1, 2, 3, 4, 5, 6});
+        int[] result = problem2070.maximumBeauty(items, new int[]{1, 2, 3, 4, 5, 6});
 
         for (int i : result) {
             System.out.println(i);
