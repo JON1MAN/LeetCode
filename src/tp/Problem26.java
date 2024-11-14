54b11aa6daa5df4d0a91a6ff1e13b41c1d5be015
aliaksei <aliaksmarchuk@gmail.com>
2024-11-14 14:14:33 +0100

    no way bro solved remove duplicate...

--- src/tp/Problem26.java
+++ src/tp/Problem26.java
@@ -1,32 +1,31 @@
 package tp;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Problem26 {
 
     public int removeDuplicates(int[] nums) {
-        int n = nums.length;
-        if(n < 1){
-            return n;
-        }
-        int count = 0;
+        int k = 1;
         int left = 0;
         int right = 1;
-
-        while(right < n){
-            if(nums[left] != nums[right]){
-                count++;
-                nums[++left] = nums[right];
+        while (left < right  && right < nums.length) {
+            if(nums[left] != nums[right]) {
+                nums[k] = nums[right];
+                left = right;
+                k++;
+                right++;
+            } else {
+                right++;
             }
-            right++;
         }
-        return count + 1;
-
+        return k - 1;
     }
 
     public static void main(String[] args) {
         Problem26 problem26 = new Problem26();
-        int[] nums = new int[]{0,0,1,1,1,2,2,3,3,4};
+        int[] nums = new int[]{1, 2};
         System.out.println(problem26.removeDuplicates(nums));
         System.out.println();
         Arrays.stream(nums)
