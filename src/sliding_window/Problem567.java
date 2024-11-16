f60af8bbd5a149c0291d0187e3fcf4f8266fa834
aliaksei <aliaksmarchuk@gmail.com>
2024-11-16 16:19:39 +0100

    another try

--- src/sliding_window/Problem567.java
+++ src/sliding_window/Problem567.java
@@ -1,19 +1,30 @@
 package sliding_window;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Problem567 {
     public boolean checkInclusion(String s1, String s2) {
         if(s1.equals(s2)) {
             return true;
         }
 
+        var array = s2.getBytes();
+        var s1_bytes = s1.getBytes();
+
+        int target = IntStream.range(0, s1_bytes.length)
+                .map(i -> s1_bytes[i])
+                .sum();
+
         int n = s2.length();
-        StringBuilder stringBuilder = new StringBuilder(s2);
-        stringBuilder.reverse();
         int left = 0;
         int right = s1.length() - 1;
 
         while (right < n) {
-            if(s1.equals(stringBuilder.substring(left, right + 1))) {
+            int sum = IntStream.range(left, right + 1)
+                    .map(i -> array[i])
+                    .sum();
+            if(target == sum) {
                 return true;
             } else {
                 right++;
@@ -25,9 +36,8 @@ public class Problem567 {
 
     public static void main(String[] args) {
         Problem567 problem567 = new Problem567();
-        String s1 = "ab";
-        String s2 = "eidbaoo";
-        System.out.println(s2.substring(0, 1));
+        String s1 = "abc";
+        String s2 = "ccccbbbbaaaa";
         System.out.println(problem567.checkInclusion(s1, s2));
     }
 }
