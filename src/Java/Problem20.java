def1b67d6cc111fd2cdaa0e2f28a815762733929
aliaksei <aliaksmarchuk@gmail.com>
2024-11-20 23:09:41 +0100

    problem 20

---
+++ src/Java/Problem20.java
@@ -0,0 +1,39 @@
+package Java;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Problem20 {
+    public boolean isValid(String s) {
+        if (s.length() == 1) {
+            return false;
+        }
+        Stack<Character> braces = new Stack<>();
+        Map<Character, Character> pairs = new HashMap<>();
+        pairs.put('{', '}');
+        pairs.put('[', ']');
+        pairs.put('(', ')');
+
+        char[] chars = s.toCharArray();
+        for (char current : chars) {
+            if (current == '}' || current == ')' || current == ']') {
+                if (braces.empty() || current != braces.peek()) {
+                    return false;
+                } else {
+                    braces.pop();
+                }
+            } else {
+                braces.push(pairs.get(current));
+            }
+        }
+
+        return braces.empty();
+    }
+
+    public static void main(String[] args) {
+        String s = "()";
+        Problem20 p = new Problem20();
+        System.out.println(p.isValid(s));
+    }
+}
