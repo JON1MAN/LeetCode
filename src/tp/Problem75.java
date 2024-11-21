f02dde7d15c57c9289064399bbaf586028aeef53
aliaksei <aliaksmarchuk@gmail.com>
2024-11-21 21:26:46 +0100

    two pointers problem 75

--- src/tp/Problem75.java
+++ src/tp/Problem75.java
@@ -1,6 +1,8 @@
 package tp;
 
 public class Problem75 {
+
+    //without two pointers
     public void sortColors(int[] nums) {
         int zero = 0;
         int one = 0;
@@ -25,6 +27,31 @@ public class Problem75 {
             } else {
                 nums[i] = 2;
                 two--;
+            }
+        }
+    }
+
+    //two pointer (three)
+    public void sortColors2(int[] nums) {
+        int left = 0;
+        int mid = 0;
+        int temp = 0;
+        int right = nums.length - 1;
+
+        while (mid <= right) {
+            if (nums[mid] == 0) {
+                temp = nums[mid];
+                nums[mid] = nums[left];
+                nums[right] = temp;
+                left++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                temp = nums[mid];
+                nums[mid] = nums[right];
+                nums[right] = temp;
+                right--;
             }
         }
     }
