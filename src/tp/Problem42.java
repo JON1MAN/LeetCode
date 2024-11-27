bd44b8613c0676e984c1058ca2129cbe627e1127
aliaksei <aliaksmarchuk@gmail.com>
2024-11-27 12:29:09 +0100

    problem 42

--- src/tp/Problem42.java
+++ src/tp/Problem42.java
@@ -5,14 +5,21 @@ public class Problem42 {
         int n = height.length;
         if (n < 3) return 0;
         int sum = 0;
-        int current = 0;
+        int[] Left = new int[n];
+        int[] Right = new int[n];
 
-        while (current < n) {
-            int left = current == 0 ? height[current] : Math.max(height[current - 1], height[current]);
-            int right = current == n - 1 ? height[current] : Math.max(height[current + 1], height[current]);
-            sum = Math.max(left, right) - height[current];
-            current++;
+        for (int i = 0; i < n; i ++) {
+            Left[i] = i == 0 ? height[i] : Math.max(height[i - 1], height[i]);
         }
+
+        for (int i = n - 1; i >= 0; i--) {
+            Right[i] = i == n - 1 ? height[i] : Math.max(height[i + 1], height[i]);
+        }
+
+        for (int i = 0; i < n; i++){
+            sum += Math.min(Right[i], Left[i]) - height[i];
+        }
+
         return sum;
     }
 }
