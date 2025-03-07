69af7a0924411a310615342dbbae829c066bc140
aliaksei <aliaksmarchuk@gmail.com>
2025-03-07 18:52:54 +0100

    763 optimized

--- src/tp/Problem763.java
+++ src/tp/Problem763.java
@@ -10,16 +10,15 @@ public class Problem763 {
         for (int i = 0; i < chars.length; i++) {
             arr[chars[i]] = i;
         }
+        List<Integer> answer = new LinkedList<>();
 
-        List<Integer> answer = new ArrayList<>();
-
-        int max = Integer.MIN_VALUE;
         int left = 0;
         int right = 0;
+        int max = Integer.MIN_VALUE;
 
         while (right < chars.length) {
             int current = arr[chars[right]];
-            max = Math.max(max, current);
+            max = Math.max(current, max);
             if (max == right) {
                 answer.add(right - left + 1);
                 left = right + 1;
